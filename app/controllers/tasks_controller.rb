fd5b0e1584222b03d8bf02e7d5d032e399904185
wa-byte <fmaro3539@gmail.com>
2020-03-28 06:19:36 +0000

    CRUD tasks2.5.3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,10 +46,10 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
+  
+ private
 
-private
-
-def task_params
+  def task_params
   params.require(:task).permit(:content)
+  end
 end
