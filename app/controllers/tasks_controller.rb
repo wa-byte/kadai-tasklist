1c28072c02091c5b63b6dfe51a842d01604dc897
wa-byte <fmaro3539@gmail.com>
2020-05-23 12:10:47 +0000

    tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   
   def index
-      @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc)
   end
 
   def show
@@ -10,11 +10,11 @@ class TasksController < ApplicationController
   end
 
   def new
-      @task = Task.new
+      @task = current_user.tasks.build
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが投稿されました'
@@ -56,6 +56,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-  params.require(:task).permit(:content, :status)
+  params.require(:task).permit(:content, :status ,:user)
   end
 end
---
+++ db/migrate/20200523025741_microposts.rb
@@ -0,0 +1,5 @@
+class Microposts < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :microposts
+  end
+end
