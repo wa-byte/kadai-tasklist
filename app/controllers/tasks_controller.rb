b9bfef6f7a86efbd834a6ea141d9550c0f5fb504
wa-byte <fmaro3539@gmail.com>
2020-05-24 09:55:13 +0000

    tasklist login 3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
       @tasks = current_user.tasks.order(id: :desc)
@@ -57,5 +58,12 @@ class TasksController < ApplicationController
 
   def task_params
   params.require(:task).permit(:content, :status ,:user)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
